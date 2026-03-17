d23b8ebf08ac4303f3aab04a1d930c3da146bf1b
Test-cmd216 <fusionedge.mailbox@gmail.com>
2026-03-17 22:53:49 +0530

    Add utils/AppError.js

---
+++ utils/AppError.js
@@ -0,0 +1,9 @@
+class AppError extends Error {
+  constructor(message, statusCode, errorCode) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.errorCode = errorCode || 'INTERNAL_ERROR';
+    this.isOperational = true;
+  }
+}
+export default AppError;
